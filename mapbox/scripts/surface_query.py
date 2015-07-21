8a65c942e987511fd600ce5efd0ec4ede36ef4b8
dnomadb <damon@mapbox.com>
2015-07-20 18:42:17 -0700

    adding utils

--- mapbox/scripts/surface_query.py
+++ mapbox/scripts/surface_query.py
@@ -1,34 +1,27 @@
 import requests
+import api_utils
 
 
 def surface(mapid, points, access_token, **kwargs):
     queryParams = {
         'access_token': access_token,
-        'points': ';'.join([
-            ','.join([
-                str(ll) for ll in pt
-            ]) for pt in points
-        ])
+        'points': api_utils.lat_lng_formatter(points)
     }
 
-    if not 'layer' in kwargs:
-        raise ValueError('<layer> must be provided')
-    else:
-        queryParams['layer'] = kwargs['layer']
-
-    if 'fields' in kwargs:
-        if type(kwargs['fields']) != type([]):
-            raise ValueError('<fields> must be a list of values')
-        else:
-            queryParams['fields'] = ','.join([
-                str(f) for f in kwargs['fields']
-                ])
-
-    if 'zoom' in kwargs:
-        if type(kwargs['zoom']) != int:
-            raise ValueError('zoom must be an integer')
-        else:
-            queryParams['zoom'] = int(kwargs['zoom'])
+    params = {
+        'layer': {
+            'required': True,
+            'type': str
+        },
+        'fields': {
+            'required': True,
+            'type': list
+        },
+        'zoom': {
+            'type': int
+        }
+    }
+    queryParams = api_utils.validator(params, kwargs, queryParams)
 
     queryUrl = 'https://api.mapbox.com/v4/surface/%s.json' % (mapid,)
 
