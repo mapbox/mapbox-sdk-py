4b3a1d004aa5431a3c425545e177abfeb94c1051
Sean Gillies <sean@mapbox.com>
2015-07-29 13:21:16 -0600

    Add a new exception class.
    100% test coverage. \m/

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,11 +10,29 @@ def test_cli_geocode_fwd():
 
     responses.add(
         responses.GET,
-        'http://api.mapbox.com/v4/geocode/mapbox.places/1600%20pennsylvania%20ave%20nw.json',
+        'http://api.mapbox.com/v4/geocode/mapbox.places/1600%20pennsylvania%20ave%20nw.json?access_token=bogus',
+        match_querystring=True,
         body='{"query": ["1600", "pennsylvania", "ave", "nw"]}', status=200,
         content_type='application/json')
 
     runner = CliRunner()
-    result = runner.invoke(main_group, ['geocode', '1600 pennsylvania ave nw'])
+    result = runner.invoke(
+        main_group,
+        ['geocode', '1600 pennsylvania ave nw', '--access-token', 'bogus'])
     assert result.exit_code == 0
     assert result.output == '{"query": ["1600", "pennsylvania", "ave", "nw"]}\n'
+
+
+@responses.activate
+def test_cli_geocode_unauthorized():
+
+    responses.add(
+        responses.GET,
+        'http://api.mapbox.com/v4/geocode/mapbox.places/1600%20pennsylvania%20ave%20nw.json',
+        body='{"message":"Not Authorized - Invalid Token"}', status=401,
+        content_type='application/json')
+
+    runner = CliRunner()
+    result = runner.invoke(main_group, ['geocode', '1600 pennsylvania ave nw'])
+    assert result.exit_code == 1
+    assert result.output == 'Error: {"message":"Not Authorized - Invalid Token"}\n'
