f0813767b0a6ab1c046aff737228529021f01b93
Matthew Perry <perrygeo@gmail.com>
2015-10-21 15:36:09 -0400

    encoding waypoints, take one

---
+++ mapbox/encoding.py
@@ -0,0 +1,64 @@
+
+def _geom_points(geom):
+    """GeoJSON geometry to a sequence of point tuples
+    """
+    if geom['type'] == 'Point':
+        yield tuple(geom['coordinates'])
+    elif geom['type'] in ('MultiPoint', 'LineString'):
+        for position in geom['coordinates']:
+            yield tuple(position)
+    else:
+        raise ValueError(
+            "Unsupported geometry type:{0}".format(geom['type']))
+
+
+def read_points(features):
+    """ Iterable of features to a sequence of point tuples
+    Where "features" can be either GeoJSON mappings
+    or objects implementing the geo_interface
+    """
+    for feature in features:
+
+        if feature['type'] == 'Feature':
+            # A GeoJSON-like mapping
+            geom = feature['geometry']
+            for pt in _geom_points(geom):
+                yield pt
+
+        elif hasattr(feature, '__geo_interface__'):
+            # An object implementing the geo_interface
+            try:
+                # Could be a Feature...
+                geom = feature.__geo_interface__['geometry']
+                for pt in _geom_points(geom):
+                    yield pt
+            except KeyError:
+                # ... or a geometry directly
+                for pt in _geom_points(feature.__geo_interface__):
+                    yield pt
+
+        else:
+            raise ValueError("Unknown object: Not a GeoJSON feature or "
+                             "an object with __geo_interface__:\n{0}".format(feature))
+
+
+def encode_waypoints(features, min_limit=None, max_limit=None, precision=6):
+    """Given an iterable of features
+    return a string encoded in waypoint-style used by certain mapbox APIs
+    ("lon,lat" pairs separated by ";")
+    """
+    coords = ['{lon:.{p}f},{lat:.{p}f}'.format(lon=lon, lat=lat, p=precision)
+              for lon, lat in read_points(features)]
+
+    if min_limit is not None and len(coords) < min_limit:
+        raise ValueError("Not enough features to encode waypoints, "
+                         "need at least {0}".format(min_limit))
+    if max_limit is not None and len(coords) > max_limit:
+        raise ValueError("Too many features to encode waypoints, "
+                         "need at most {0}".format(max_limit))
+
+    return ';'.join(coords)
+
+
+# TODO
+# def encode_polyline(features, zoom_level=18):
---
+++ tests/test_encoding.py
@@ -0,0 +1,86 @@
+import pytest
+from mapbox.encoding import read_points, encode_waypoints
+
+
+gj_point_features = [{
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -87.33787536621092,
+            36.539156961321574]}}, {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -88.2476806640625,
+            36.92217534275667]}}]
+
+
+gj_multipoint_features = [{
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "MultiPoint",
+        "coordinates": [
+            [-87.33787536621092,
+             36.539156961321574],
+            [-88.2476806640625,
+             36.92217534275667]]}}]
+
+
+gj_line_features = [{
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+        "type": "LineString",
+        "coordinates": [
+            [-87.33787536621092,
+             36.539156961321574],
+            [-88.2476806640625,
+             36.92217534275667]]}}]
+
+
+def test_read_geojson_features():
+    expected = [(-87.33787536621092, 36.539156961321574),
+                (-88.2476806640625, 36.92217534275667)]
+
+    assert expected == list(read_points(gj_point_features))
+    assert expected == list(read_points(gj_multipoint_features))
+    assert expected == list(read_points(gj_line_features))
+
+
+def test_encode_waypoints():
+    expected = "-87.337875,36.539157;-88.247681,36.922175"
+
+    assert expected == encode_waypoints(gj_point_features)
+    assert expected == encode_waypoints(gj_multipoint_features)
+    assert expected == encode_waypoints(gj_line_features)
+
+
+def test_encode_limits():
+    expected = "-87.337875,36.539157;-88.247681,36.922175"
+    assert expected == encode_waypoints(gj_point_features)
+    with pytest.raises(ValueError) as exc:
+        encode_waypoints(gj_point_features, min_limit=3)
+    assert 'at least' in str(exc.value)
+
+    with pytest.raises(ValueError) as exc:
+        encode_waypoints(gj_point_features, max_limit=1)
+    assert 'at most' in str(exc.value)
+
+
+# TODO
+# def test_unsupported_geometry():
+
+# TODO
+# def test_unknown_object():
+
+# TODO
+# def test_encode_polyline():
+#     expected = "vdatOwp_~EhupD{xiA"
+#     assert expected == encode_polyline(gj_point_features)
+#     assert expected == encode_polyline(gj_multipoint_features)
+#     assert expected == encode_polyline(gj_line_features)
