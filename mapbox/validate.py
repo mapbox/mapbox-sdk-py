1a7474419964cc29cccedf12c3e61b322ae0078c
Matthew Perry <perrygeo@gmail.com>
2015-12-14 07:30:27 -0400

    image size

--- mapbox/validate.py
+++ mapbox/validate.py
@@ -8,3 +8,8 @@ def lon(val):
     if val is not None and (val < -180 or val > 180):
         raise ValueError("Longitude must be between -180 and 180")
     return val
+
+def size(val):
+    if not (1 < val[0] < 1280) and not (1 < val[1] < 1280):
+        raise ValueError("Image height and width must be between 1 and 1280")
+    return val
