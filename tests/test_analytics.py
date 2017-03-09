fad3990badafc5f72147aaca4141131ab51a1904
amishas157 <amishas157@gmail.com>
2017-03-09 17:13:28 +0530

    improve test

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -1,5 +1,7 @@
 import pytest
 
+import responses
+
 import mapbox
 from mapbox import errors
 
@@ -14,33 +16,53 @@ def test_profile_valid(resource_type):
     assert resource_type == mapbox.Analytics(
         access_token='pk.test')._validate_resource_type(resource_type)
 
-def test_period_invalid():
-	with pytest.raises(errors.InvalidPeriodError):
-		mapbox.Analytics(access_token='pk.test')._validate_period('2016-03-22T00:00:00.000Z', None)
+@pytest.mark.parametrize(
+    'start, end',[('2016-03-22T00:00:00.000Z', None),
+                ('2016-03-22T00:00:00.000Z', '2016-03-20T00:00:00.000Z'),
+                ('2016-03-22T00:00:00.000Z', '2017-04-20T00:00:00.000Z')])
+def test_period_invalid(start, end):
+    with pytest.raises(errors.InvalidPeriodError):
+        mapbox.Analytics(access_token='pk.test')._validate_period(start, end)
 
-	with pytest.raises(errors.InvalidPeriodError):
-		mapbox.Analytics(access_token='pk.test')._validate_period('2016-03-22T00:00:00.000Z', '2016-03-20T00:00:00.000Z')
-
-	with pytest.raises(errors.InvalidPeriodError):
-		mapbox.Analytics(access_token='pk.test')._validate_period('2016-03-22T00:00:00.000Z', '2017-04-20T00:00:00.000Z')
-
-def test_period_valid():
-	start = '2016-03-22T00:00:00.000Z'
-	end = '2016-03-24T00:00:00.000Z'
-	period = start, end
-	assert period == mapbox.Analytics(access_token='pk.test')._validate_period(start, end)
-
-	start = None
-	end = None
-	period = start, end
-	assert period == mapbox.Analytics(access_token='pk.test')._validate_period(start, end)
+@pytest.mark.parametrize(
+    'start, end', [('2016-03-22T00:00:00.000Z', '2016-03-24T00:00:00.000Z'),
+                   (None, None)])
+def test_period_valid(start, end):
+    period = start, end
+    assert period == mapbox.Analytics(access_token='pk.test')._validate_period(start, end)
 
 def test_username_invalid():
-	"""Username is requird."""
-	with pytest.raises(errors.InvalidUsernameError):
-		mapbox.Analytics(access_token='pk.test')._validate_username(None)
+    """Username is requird."""
+    with pytest.raises(errors.InvalidUsernameError):
+        mapbox.Analytics(access_token='pk.test')._validate_username(None)
 
 def test_username_valid():
-	"""Providing valid username"""
-	user = 'abc'
-	assert user == mapbox.Analytics(access_token='pk.test')._validate_username(user)
+    """Providing valid username"""
+    user = 'abc'
+    assert user == mapbox.Analytics(access_token='pk.test')._validate_username(user)
+
+def test_id_invalid():
+    """id is required when resource type is other than accounts"""
+    resource_type = 'tokens'
+    id = None
+    with pytest.raises(errors.InvalidId):
+        mapbox.Analytics(access_token='pk.test')._validate_id(resource_type, id)
+
+@pytest.mark.parametrize(
+    'resource_type, id', [('accounts', None),
+                   ('tokens', 'abc')])
+def test_id_valid(resource_type, id):
+    """id is not required when resource type is accounts"""
+    assert id == mapbox.Analytics(access_token='pk.test')._validate_id(resource_type, id)
+
+@responses.activate
+def test_analytics():
+    responses.add(
+        responses.GET,
+        'https://api.mapbox.com/analytics/v1/accounts/sanjayb?access_token=pk.test&id=abc&period=2016-03-22T00%3A00%3A00.000Z%2C2016-03-24T00%3A00%3A00.000Z',
+        match_querystring=True,
+        body='{"key": "value"}',
+        status=200)
+
+    res = mapbox.Analytics(access_token='pk.test').analytics('accounts', 'sanjayb', 'abc', '2016-03-22T00:00:00.000Z', '2016-03-24T00:00:00.000Z')
+    assert res.status_code == 200
