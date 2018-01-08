d29f0c4bf475f3a126b61e89bcd1336fec5a7675
Sean Gillies <sean.gillies@gmail.com>
2018-01-08 10:57:42 -0700

    Add class for the Matrix API V1 (#215)
    * Add class for the Matrix API V1
    
    Resolves #212
    
    * Add a second response for Python 2.7
    
    * distance -> matrix
    
    * Oops, -88.0 not -8.0
    
    * Add sources and destinations params to matrix()
    
    Also validate profiles as we will for directions v5.
    
    * Add temp support for distance v1 profiles

--- mapbox/__init__.py
+++ mapbox/__init__.py
@@ -1,5 +1,5 @@
 # mapbox
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 
 from .services.datasets import Datasets
 from .services.directions import Directions
@@ -7,6 +7,7 @@ from .services.distance import Distance
 from .services.geocoding import (
     Geocoder, InvalidCountryCodeError, InvalidPlaceTypeError)
 from .services.mapmatching import MapMatcher
+from .services.matrix import DirectionsMatrix
 from .services.surface import Surface
 from .services.static import Static
 from .services.static_style import StaticStyle
