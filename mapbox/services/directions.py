b9bf3b3260ce3c0300a2a0f6379a9b0b30bb691b
Matthew Perry <perrygeo@users.noreply.github.com>
2018-02-22 18:12:27 +0530

    Directions v5  (#203)
    * basic v5 url scheme with new profiles
    
    * s/geometry/geometries for consistency with http api docs
    
    * add stubs for all new v5 parameters
    
    * implement bearings, test coverage to 100
    
    * use coordinate terminology in directions, warn on v4 profile
    
    * enforce that bearings and radiuses are both specified
    
    * convert now-default polyline geometries to geojson
    
    * add a combined waypoint_snapping parameter
    
    * v5 urls
    
    * fix validation logic to not rely on TypeError, explict type checks
    
    * Update geocoding test, fix docstring

--- mapbox/services/directions.py
+++ mapbox/services/directions.py
@@ -1,124 +1,272 @@
+import warnings
+from numbers import Number
+
+import polyline
 from uritemplate import URITemplate
 
-from mapbox.encoding import encode_waypoints
+from mapbox.encoding import encode_waypoints as encode_coordinates
 from mapbox.services.base import Service
 from mapbox import errors
 
 
 class Directions(Service):
-    """Access to the Directions API V4"""
+    """Access to the Directions v5 API."""
 
     api_name = 'directions'
-    api_version = 'v4'
+    api_version = 'v5'
 
-    valid_profiles = ['mapbox.driving',
-                      'mapbox.cycling',
-                      'mapbox.walking']
-    valid_instruction_formats = ['text', 'html']
-    valid_geom_encoding = ['geojson', 'polyline', 'false']
+    valid_profiles = [
+        'mapbox/driving',
+        'mapbox/driving-traffic',
+        'mapbox/walking',
+        'mapbox/cycling']
+    valid_geom_encoding = ['geojson', 'polyline', 'polyline6']
+    valid_geom_overview = ['full', 'simplified', False]
+    valid_annotations = ['duration', 'distance', 'speed']
 
     @property
     def baseuri(self):
-        return 'https://{0}/{2}/{1}'.format(
+        return 'https://{0}/{1}/{2}'.format(
             self.host, self.api_name, self.api_version)
 
     def _validate_profile(self, profile):
+        # Backwards compatible with v4 profiles
+        v4_to_v5_profiles = {
+            'mapbox.driving': 'mapbox/driving',
+            'mapbox.cycling': 'mapbox/cycling',
+            'mapbox.walking': 'mapbox/walking'}
+        if profile in v4_to_v5_profiles:
+            profile = v4_to_v5_profiles[profile]
+            warnings.warn('Converting v4 profile to v5, use {} instead'.format(profile),
+                          errors.MapboxDeprecationWarning)
         if profile not in self.valid_profiles:
             raise errors.InvalidProfileError(
                 "{0} is not a valid profile".format(profile))
         return profile
 
+    def _validate_annotations(self, annotations):
+        results = []
+        if annotations is None:
+            return None
+        for annotation in annotations:
+            if annotation not in self.valid_annotations:
+                raise errors.InvalidParameterError(
+                    "{0} is not a valid annotation".format(annotation))
+            else:
+                results.append(annotation)
+        return results
+
     def _validate_geom_encoding(self, geom_encoding):
         if geom_encoding is not None and \
            geom_encoding not in self.valid_geom_encoding:
             raise errors.InvalidParameterError(
-                "{0} is not a valid geometry encoding".format(geom_encoding))
+                "{0} is not a valid geometry format".format(geom_encoding))
         return geom_encoding
 
-    def _validate_instruction_format(self, instruction_format):
-        if instruction_format is not None and \
-           instruction_format not in self.valid_instruction_formats:
+    def _validate_geom_overview(self, overview):
+        if overview is not None and overview not in self.valid_geom_overview:
             raise errors.InvalidParameterError(
-                "{0} is not a valid instruction format".format(
-                    instruction_format))
-        return instruction_format
+                "{0} is not a valid geometry overview type".format(overview))
+        return overview
 
-    def directions(self, features, profile='mapbox.driving', alternatives=None,
-                   instructions=None, geometry=None, steps=None):
+    def _validate_snapping(self, snaps, features):
+        bearings = []
+        radii = []
+        if snaps is None:
+            return (None, None)
+        if len(snaps) != len(features):
+            raise errors.InvalidParameterError(
+                'Must provide exactly one snapping element for each input feature')
+        for snap in snaps:
+            if snap is None:
+                bearings.append(None)
+                radii.append(None)
+            else:
+                try:
+                    # radius-only
+                    radius = self._validate_radius(snap)
+                    bearing = None
+                except errors.InvalidParameterError:
+                    # (radius, angle, range) tuple
+                    try:
+                        radius, angle, rng = snap
+                    except ValueError:
+                        raise errors.InvalidParameterError(
+                            'waypoint snapping should contain 3 elements: '
+                            '(bearing, angle, range)')
+                    self._validate_radius(radius)
+
+                    try:
+                        assert angle >= 0
+                        assert angle <= 360
+                        assert rng >= 0
+                        assert rng <= 360
+                    except (TypeError, AssertionError):
+                        raise errors.InvalidParameterError(
+                            'angle and range must be between 0 and 360')
+                    bearing = (angle, rng)
+
+                bearings.append(bearing)
+                radii.append(radius)
+
+        if all([b is None for b in bearings]):
+            bearings = None
+
+        return (bearings, radii)
+
+    def _validate_radius(self, radius):
+        if radius is None:
+            return None
+
+        if isinstance(radius, str):
+            if radius != 'unlimited':
+                raise errors.InvalidParameterError(
+                    '{0} is not a valid radius'.format(radius))
+        elif isinstance(radius, Number):
+            if radius <= 0:
+                raise errors.InvalidParameterError(
+                    'radius must be greater than zero'.format(radius))
+        else:
+            raise errors.InvalidParameterError(
+                '{0} is not a valid radius'.format(radius))
+
+        return radius
+
+    @staticmethod
+    def _encode_bearing(b):
+        if b is None:
+            return ''
+        else:
+            return '{},{}'.format(*b)
+
+    def directions(self, features, profile='mapbox/driving',
+                   alternatives=None, geometries=None, overview=None, steps=None,
+                   continue_straight=None, waypoint_snapping=None, annotations=None,
+                   language=None, **kwargs):
         """Request directions for waypoints encoded as GeoJSON features.
 
-		Parameters
-		----------
-		features : list
-			List of GeoJSON features.
-		profile : str
-			Name of a Mapbox profile such as 'mapbox.driving'.
-		alternatives : bool
+        Parameters
+        ----------
+        features : iterable
+            An collection of GeoJSON features
+        profile : str
+            Name of a Mapbox profile such as 'mapbox.driving'
+        alternatives : bool
+            Whether to try to return alternative routes, default: False
+        geometries : string
+            Type of geometry returned (geojson, polyline, polyline6)
+        overview : string or False
+            Type of returned overview geometry: 'full', 'simplified',
+            or False
+        steps : bool
+            Whether to return steps and turn-by-turn instructions,
+            default: False
+        continue_straight : bool
+            Direction of travel when departing intermediate waypoints
+        radiuses : iterable of numbers or 'unlimited'
+            Must be same length as features
+        waypoint_snapping : list
+            Controls snapping of waypoints
 
-		instructions : str
+            The list is zipped with the features collection and must
+            have the same length. Elements of the list must be one of:
 
-		geometry : str
+            - A number (interpretted as a snapping radius)
+            - The string 'unlimited' (unlimited snapping radius)
+            - A 3-element tuple consisting of (radius, angle, range)
+            - None (no snapping parameters specified for that waypoint)
+        annotations : str
+            Whether or not to return additional metadata along the route
 
-		steps : bool
+            Possible values are: 'duration', 'distance', 'speed', and
+            'congestion'. Several annotations can be used by joining
+            them with ','.
+        language : str
+            Language of returned turn-by-turn text instructions,
+            default: 'en'
 
-
-		Returns
-		-------
-		response
-			It returns a response object with a geojson() method for accessing the route(s) as a GeoJSON-like FeatureCollection dictionary.
+        Returns
+        -------
+        requests.Response
+            The response object has a geojson() method for access to
+            the route(s) as a GeoJSON-like FeatureCollection
+            dictionary.
         """
+        # backwards compatible, deprecated
+        if 'geometry' in kwargs and geometries is None:
+            geometries = kwargs['geometry']
+            warnings.warn('Use `geometries` instead of `geometry`',
+                          errors.MapboxDeprecationWarning)
+
+        annotations = self._validate_annotations(annotations)
+        coordinates = encode_coordinates(
+            features, precision=6, min_limit=2, max_limit=25)
+        geometries = self._validate_geom_encoding(geometries)
+        overview = self._validate_geom_overview(overview)
         profile = self._validate_profile(profile)
-        instructions = self._validate_instruction_format(instructions)
-        geometry = self._validate_geom_encoding(geometry)
-        waypoints = encode_waypoints(features, precision=6,
-                                     min_limit=2, max_limit=30)
+
+        bearings, radii = self._validate_snapping(waypoint_snapping, features)
 
         params = {}
         if alternatives is not None:
             params.update(
                 {'alternatives': 'true' if alternatives is True else 'false'})
-        if instructions is not None:
-            params.update({'instructions': instructions})
-        if geometry is not None:
+        if geometries is not None:
+            params.update({'geometries': geometries})
+        if overview is not None:
             params.update(
-                {'geometry': 'false' if geometry is False else geometry})
+                {'overview': 'false' if overview is False else overview})
         if steps is not None:
             params.update(
                 {'steps': 'true' if steps is True else 'false'})
+        if continue_straight is not None:
+            params.update(
+                {'continue_straight': 'true' if steps is True else 'false'})
+        if annotations is not None:
+            params.update({'annotations': ','.join(annotations)})
+        if language is not None:
+            params.update({'language': language})
+        if radii is not None:
+            params.update(
+                {'radiuses': ';'.join(str(r) for r in radii)})
+        if bearings is not None:
+            params.update(
+                {'bearings': ';'.join(self._encode_bearing(b) for b in bearings)})
 
-        uri = URITemplate(self.baseuri + '/{profile}/{waypoints}.json').expand(
-            profile=profile, waypoints=waypoints)
+        profile_ns, profile_name = profile.split('/')
+
+        uri = URITemplate(
+            self.baseuri + '/{profile_ns}/{profile_name}/{coordinates}.json').expand(
+                profile_ns=profile_ns, profile_name=profile_name, coordinates=coordinates)
 
         resp = self.session.get(uri, params=params)
         self.handle_http_error(resp)
 
         def geojson():
-            return self._geojson(resp.json())
-
+            return self._geojson(resp.json(), geom_format=geometries)
         resp.geojson = geojson
         return resp
 
-    def _geojson(self, data):
+    def _geojson(self, data, geom_format=None):
         fc = {
             'type': 'FeatureCollection',
             'features': []}
 
         for route in data['routes']:
+            if geom_format == 'geojson':
+                geom = route['geometry']
+            else:
+                # convert default polyline encoded geometry
+                geom = {
+                    'type': 'LineString',
+                    'coodinates': polyline.decode(route['geometry'])}
 
             feature = {
                 'type': 'Feature',
+                'geometry': geom,
                 'properties': {
-                    # TODO handle these nested structures
-                    # Flatten or ???
-                    # 'destination': data['destination'],
-                    # 'origin': data['origin'],
-                    # 'waypoints': data['waypoints'],
-                    # 'steps': route['steps']
+                    # TODO include RouteLegs and other details
                     'distance': route['distance'],
-                    'duration': route['duration'],
-                    'summary': route['summary']}}
-
-            feature['geometry'] = route['geometry']
+                    'duration': route['duration']}}
             fc['features'].append(feature)
-
         return fc
--- tests/test_directions.py
+++ tests/test_directions.py
@@ -25,7 +25,7 @@ def test_class_attrs():
     """Get expected class attr values"""
     serv = mapbox.Directions()
     assert serv.api_name == 'directions'
-    assert serv.api_version == 'v4'
+    assert serv.api_version == 'v5'
 
 
 @responses.activate
@@ -36,15 +36,15 @@ def test_directions(cache):
 
     responses.add(
         responses.GET,
-        'https://api.mapbox.com/v4/directions/mapbox.driving/-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test',
+        'https://api.mapbox.com/directions/v5/mapbox/driving/' +
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test',
         match_querystring=True,
         body=body, status=200,
         content_type='application/json')
 
     res = mapbox.Directions(access_token='pk.test', cache=cache).directions(points)
     assert res.status_code == 200
-    assert sorted(res.json()['routes'][0].keys()) == ['distance', 'duration', 'geometry', 'steps', 'summary']
-    assert sorted(res.json().keys()) == ['destination', 'origin', 'routes', 'waypoints']
+    assert 'distance' in res.json()['routes'][0].keys()
 
 
 @responses.activate
@@ -54,7 +54,8 @@ def test_directions_geojson():
 
     responses.add(
         responses.GET,
-        'https://api.mapbox.com/v4/directions/mapbox.driving/-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test',
+        'https://api.mapbox.com/directions/v5/mapbox/driving/'
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test',
         match_querystring=True,
         body=body, status=200,
         content_type='application/json')
@@ -62,23 +63,46 @@ def test_directions_geojson():
     res = mapbox.Directions(access_token='pk.test').directions(points)
     fc = res.geojson()
     assert fc['type'] == 'FeatureCollection'
-    assert sorted(fc['features'][0]['properties'].keys()) == ['distance', 'duration', 'summary']
-    assert fc['features'][0]['geometry']['type'] == "LineString"
+    assert fc['features'][0]['geometry']['type'] == 'LineString'
+
+
+@responses.activate
+def test_directions_geojson_as_geojson():
+    with open('tests/moors_geojson.json') as fh:
+        body = fh.read()
+
+    responses.add(
+        responses.GET,
+        'https://api.mapbox.com/directions/v5/mapbox/driving/'
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test'
+        '&geometries=geojson',
+        match_querystring=True,
+        body=body, status=200,
+        content_type='application/json')
+
+    res = mapbox.Directions(access_token='pk.test').directions(
+        points, geometries='geojson')
+    fc = res.geojson()
+    assert fc['type'] == 'FeatureCollection'
+    assert fc['features'][0]['geometry']['type'] == 'LineString'
 
 
 def test_invalid_profile():
     with pytest.raises(ValueError):
         mapbox.Directions(access_token='pk.test').directions(
-            None, profile='bogus')
+            points, profile='bogus')
 
 
 @responses.activate
 def test_direction_params():
-    params = "&alternatives=false&instructions=html&geometry=polyline&steps=false"
+    params = "&alternatives=false&geometries=polyline&overview=false&steps=false" \
+             "&continue_straight=false&annotations=distance%2Cspeed&language=en" \
+             "&radiuses=10%3Bunlimited"
 
     responses.add(
         responses.GET,
-        'https://api.mapbox.com/v4/directions/mapbox.driving/-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test' + params,
+        'https://api.mapbox.com/directions/v5/mapbox/driving/'
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test' + params,
         match_querystring=True,
         body="not important, only testing URI templating", status=200,
         content_type='application/json')
@@ -86,9 +110,69 @@ def test_direction_params():
     res = mapbox.Directions(access_token='pk.test').directions(
         points,
         alternatives=False,
-        instructions='html',
-        geometry='polyline',
+        geometries='polyline',
+        overview=False,
+        continue_straight=True,
+        annotations=['distance', 'speed'],
+        language='en',
+        waypoint_snapping=[10, 'unlimited'],
         steps=False)
+    assert res.status_code == 200
+
+
+@responses.activate
+def test_direction_backwards_compat():
+    """Ensure old calls to directions method work against v5 API
+    """
+    responses.add(
+        responses.GET,
+        'https://api.mapbox.com/directions/v5/mapbox/cycling/'
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test'
+        '&geometries=polyline',
+        match_querystring=True,
+        body="not important, only testing URI templating", status=200,
+        content_type='application/json')
+
+    res = mapbox.Directions(access_token='pk.test').directions(
+        points,
+        geometry='polyline',   # plural in v5
+        profile='mapbox.cycling',  # '/' delimited in v5
+    )
+    # TODO instructions parameter removed in v5
+    assert res.status_code == 200
+
+
+@responses.activate
+def test_direction_bearings():
+    responses.add(
+        responses.GET,
+        'https://api.mapbox.com/directions/v5/mapbox/driving/'
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test'
+        '&radiuses=10%3B20&bearings=270%2C45%3B315%2C90',
+        match_querystring=True,
+        body="not important, only testing URI templating", status=200,
+        content_type='application/json')
+
+    res = mapbox.Directions(access_token='pk.test').directions(
+        points,
+        waypoint_snapping=[(10, 270, 45), (20, 315, 90)])
+    assert res.status_code == 200
+
+
+@responses.activate
+def test_direction_bearings_none():
+    responses.add(
+        responses.GET,
+        'https://api.mapbox.com/directions/v5/mapbox/driving/'
+        '-87.337875%2C36.539157%3B-88.247681%2C36.922175.json?access_token=pk.test'
+        '&radiuses=10%3B20&bearings=%3B315%2C90',
+        match_querystring=True,
+        body="not important, only testing URI templating", status=200,
+        content_type='application/json')
+
+    res = mapbox.Directions(access_token='pk.test').directions(
+        points,
+        waypoint_snapping=[10, (20, 315, 90)])
     assert res.status_code == 200
 
 
@@ -98,7 +182,77 @@ def test_invalid_geom_encoding():
         service._validate_geom_encoding('wkb')
 
 
-def test_invalid_instruction_format():
+def test_v4_profile_aliases():
     service = mapbox.Directions(access_token='pk.test')
-    with pytest.raises(mapbox.errors.ValidationError):
-        service._validate_instruction_format('markdown')
+    assert 'mapbox/cycling' == service._validate_profile('mapbox.cycling')
+
+
+def test_invalid_annotations():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError):
+        service._validate_annotations(['awesomeness'])
+
+
+def test_invalid_geom_overview():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError):
+        service._validate_geom_overview('infinite')
+
+
+def test_invalid_radiuses():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError) as e:
+        service._validate_radius('forever')
+        assert 'not a valid radius' in str(e)
+
+
+def test_invalid_number_of_bearings():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError) as e:
+        service._validate_snapping([1, 2, 3], points)
+        assert 'exactly one' in str(e)
+
+
+def test_invalid_bearing_tuple():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError) as e:
+        service._validate_snapping([(270, 45, 'extra'), (315,)], points)
+        assert 'bearing tuple' in str(e)
+
+
+def test_snapping_bearing_none():
+    service = mapbox.Directions(access_token='pk.test')
+    bearings, radii = service._validate_snapping([(10, 270, 45), None], points)
+    assert bearings == [(270, 45), None]
+    assert radii == [10, None]
+
+
+def test_snapping_radii_none():
+    service = mapbox.Directions(access_token='pk.test')
+    bearings, radii = service._validate_snapping([(10, 270, 45), None], points)
+    assert bearings == [(270, 45), None]
+    assert radii == [10, None]
+
+
+def test_validate_radius_none():
+    service = mapbox.Directions(access_token='pk.test')
+    assert service._validate_radius(None) is None
+
+
+def test_validate_radius_invalid():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError) as e:
+        service._validate_radius(-1)
+
+
+def test_invalid_bearing_domain():
+    service = mapbox.Directions(access_token='pk.test')
+    with pytest.raises(mapbox.errors.InvalidParameterError) as e:
+        service._validate_snapping([(-1, 90), (315, 90)], points)
+        assert 'between 0 and 360' in str(e)
+
+
+def test_bearings_without_radius():
+    with pytest.raises(TypeError):
+        mapbox.Directions(access_token='pk.test').directions(
+            waypoint_snapping=[(270, 45), (270, 45)])
