c44088fba0d0c2c460fab50916001fe0e61ed3c1
critical-path <37241479+critical-path@users.noreply.github.com>
2018-05-26 12:37:33 -0400

    Update errors.py

--- mapbox/errors.py
+++ mapbox/errors.py
@@ -82,10 +82,6 @@ class InvalidFileFormatError(ValidationError):
     pass
 
 
-class InvalidOptionError(ValidationError):
-    pass
-
-
 class InvalidFeatureFormatError(ValidationError):
     pass
 
