3b67bdc774ab1f1b62a4af1f5f5c4061712be382
critical-path <37241479+critical-path@users.noreply.github.com>
2018-07-10 19:22:34 -0400

    Update test_tilesets.py

--- tests/test_tilesets.py
+++ tests/test_tilesets.py
@@ -9,7 +9,10 @@ from mapbox.services.tilesets import Tilesets
 
 from base64 import b64encode
 
-from pytest import raises
+from pytest import (
+    mark, 
+    raises
+)
 
 from responses import (
     activate,
@@ -17,8 +20,10 @@ from responses import (
     GET
 )
 
+
 USERNAME = b64encode(b"{\"u\":\"user\"}").decode("utf-8")
 ACCESS_TOKEN = "pk.{}.test".format(USERNAME)
+
 
 def test_object_attributes():
     tilesets = Tilesets()
@@ -30,92 +35,75 @@ def test_object_attributes():
     assert tilesets.valid_sortbys
     assert tilesets.base_uri
 
-def test_validate_tileset_type():
-    tilesets = Tilesets()
 
-    # invalid value
+def test_validate_tileset_type_invalid():
+    tilesets = Tilesets()
 
     with raises(InvalidTilesetTypeError) as exception:
         tileset_type = "invalid"
         result = tilesets._validate_tileset_type(tileset_type)
 
-    # valid values
 
-    tileset_types = [
-        "raster",
-        "vector"
-    ]
-
-    for tileset_type in tileset_types:
-        result = tilesets._validate_tileset_type(tileset_type)
-        assert result == tileset_type
-
-def test_validate_visibility():
+@mark.parametrize("tileset_type", ["raster", "vector"])
+def test_validate_tileset_type_valid(tileset_type):
     tilesets = Tilesets()
+    result = tilesets._validate_tileset_type(tileset_type)
+    assert result == tileset_type
 
-    # invalid value
+
+def test_validate_visibility_invalid():
+    tilesets = Tilesets()
 
     with raises(InvalidVisibilityError) as exception:
         visibility = "invalid"
         result = tilesets._validate_visibility(visibility)
 
-    # valid values
 
-    visibilities = [
-        "private",
-        "public"
-    ]
-
-    for visibility in visibilities:
-        result = tilesets._validate_visibility(visibility)
-        assert result == visibility
-
-def test_validate_sortby():
+@mark.parametrize("visibility", ["private", "public"])
+def test_validate_visibility_valid(visibility):
     tilesets = Tilesets()
+    result = tilesets._validate_visibility(visibility)
+    assert result == visibility
 
-    # invalid value
+
+def test_validate_sortby_invalid():
+    tilesets = Tilesets()
 
     with raises(InvalidSortbyError) as exception:
         sortby = "invalid"
         result = tilesets._validate_sortby(sortby)
 
-    # valid values
 
-    sortbys = [
-        "created",
-        "modified"
-    ]
+@mark.parametrize("sortby", ["created", "modified"])
+def test_validate_sortby_valid(sortby):
+    tilesets = Tilesets()
+    result = tilesets._validate_sortby(sortby)
+    assert result == sortby
 
-    for sortby in sortbys:
-        result = tilesets._validate_sortby(sortby)
-        assert result == sortby
 
-def test_validate_limit():
+@mark.parametrize("limit", [0, 501])
+def test_validate_limit_invalid(limit):
     tilesets = Tilesets()
 
-    # invalid value
-
     with raises(InvalidLimitError) as exception:
-        limit = 0
         result = tilesets._validate_limit(limit)
 
-    # invalid value
 
-    with raises(InvalidLimitError) as exception:
-        limit = 501
-        result = tilesets._validate_limit(limit)
-
-    # valid value
-
-    limit = 100
+@mark.parametrize("limit", [1, 250, 500])
+def test_validate_limit_valid(limit):
+    tilesets = Tilesets()
     result = tilesets._validate_limit(limit)
     assert result == limit
+
 
 @activate
 def test_tilesets():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN),
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
@@ -125,103 +113,97 @@ def test_tilesets():
     response = tilesets.tilesets()
     assert response.status_code == 200
 
-@activate
-def test_tilesets_with_tileset_type():
-    add(
-        method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&type=raster".format(ACCESS_TOKEN),
-        match_querystring=True,
-        body="{\"key\": \"value\"}",
-        status=200
-    )
-
-    tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(tileset_type="raster")
-    assert response.status_code == 200
-
-    add(
-        method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&type=vector".format(ACCESS_TOKEN),
-        match_querystring=True,
-        body="{\"key\": \"value\"}",
-        status=200
-    )
-
-    tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(tileset_type="vector")
-    assert response.status_code == 200
 
 @activate
-def test_tilesets_with_visibility():
+@mark.parametrize("tileset_type", ["raster", "vector"])
+def test_tilesets_with_tileset_type(tileset_type):
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&visibility=private".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&type={}".format(tileset_type),
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
     )
 
     tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(visibility="private")
+    response = tilesets.tilesets(tileset_type=tileset_type)
     assert response.status_code == 200
 
-    add(
-        method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&visibility=public".format(ACCESS_TOKEN),
-        match_querystring=True,
-        body="{\"key\": \"value\"}",
-        status=200
-    )
-
-    tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(visibility="public")
-    assert response.status_code == 200
 
 @activate
-def test_tilesets_with_sortby():
+@mark.parametrize("visibility", ["private", "public"])
+def test_tilesets_with_visibility(visibility):
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&sortby=created".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&visibility={}".format(visibility),
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
     )
 
     tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(sortby="created")
+    response = tilesets.tilesets(visibility=visibility)
     assert response.status_code == 200
 
-    add(
-        method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&sortby=modified".format(ACCESS_TOKEN),
-        match_querystring=True,
-        body="{\"key\": \"value\"}",
-        status=200
-    )
-
-    tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(sortby="modified")
-    assert response.status_code == 200
 
 @activate
-def test_tilesets_with_limit():
+@mark.parametrize("sortby", ["created", "modified"])
+def test_tilesets_with_sortby(sortby):
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&limit=500".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&sortby={}".format(sortby),
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
     )
 
     tilesets = Tilesets(access_token=ACCESS_TOKEN)
-    response = tilesets.tilesets(limit=500)
+    response = tilesets.tilesets(sortby=sortby)
     assert response.status_code == 200
+
+
+@activate
+@mark.parametrize("limit", [1, 250, 500])
+def test_tilesets_with_limit(limit):
+    add(
+        method=GET,
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&limit={}".format(limit),
+        match_querystring=True,
+        body="{\"key\": \"value\"}",
+        status=200
+    )
+
+    tilesets = Tilesets(access_token=ACCESS_TOKEN)
+    response = tilesets.tilesets(limit=limit)
+    assert response.status_code == 200
+
 
 @activate
 def test_tilesets_with_tileset_type_and_visibility():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&type=vector&visibility=public".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&type=vector" +
+        "&visibility=public",
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
@@ -231,11 +213,17 @@ def test_tilesets_with_tileset_type_and_visibility():
     response = tilesets.tilesets(tileset_type="vector", visibility="public")
     assert response.status_code == 200
 
+
 @activate
 def test_tilesets_with_tileset_type_and_sortby():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&type=vector&sortby=created".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&type=vector" +
+        "&sortby=created",
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
@@ -245,11 +233,17 @@ def test_tilesets_with_tileset_type_and_sortby():
     response = tilesets.tilesets(tileset_type="vector", sortby="created")
     assert response.status_code == 200
 
+
 @activate
 def test_tilesets_with_tileset_type_and_limit():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&type=vector&limit=500".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&type=vector" +
+        "&limit=500",
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
@@ -259,11 +253,17 @@ def test_tilesets_with_tileset_type_and_limit():
     response = tilesets.tilesets(tileset_type="vector", limit=500)
     assert response.status_code == 200
 
+
 @activate
 def test_tilesets_with_visibility_and_sortby():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&visibility=public&sortby=created".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&visibility=public" +
+        "&sortby=created",
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
@@ -273,11 +273,17 @@ def test_tilesets_with_visibility_and_sortby():
     response = tilesets.tilesets(visibility="public", sortby="created")
     assert response.status_code == 200
 
+
 @activate
 def test_tilesets_with_visibility_and_limit():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&visibility=public&limit=500".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user" +
+        "?access_token={}".format(ACCESS_TOKEN) +
+        "&visibility=public" +
+        "&limit=500",
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
@@ -287,11 +293,16 @@ def test_tilesets_with_visibility_and_limit():
     response = tilesets.tilesets(visibility="public", limit=500)
     assert response.status_code == 200
 
+
 @activate
 def test_tilesets_with_sortby_and_limit():
     add(
         method=GET,
-        url="https://api.mapbox.com/tilesets/v1/user?access_token={}&sortby=created&limit=500".format(ACCESS_TOKEN),
+        url="https://api.mapbox.com" +
+        "/tilesets/v1" +
+        "/user?access_token={}".format(ACCESS_TOKEN) +
+        "&sortby=created" +
+        "&limit=500",
         match_querystring=True,
         body="{\"key\": \"value\"}",
         status=200
